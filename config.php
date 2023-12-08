97326cfe2c59d8a537b62aaaeb7e4e765d7280a4
sdlima4025 <sergiolimafg3@gmail.com>
2023-12-08 10:51:07 -0300

    criado conexão DB devsbook com pdo

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$base = 'http://localhost/devsbook_oo';
+
+$db_name = 'devsbook_oo';
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+
+$pdo = new PDO("mysql:dename=".$db_name.";host=".$db_host, $db_user, $db_pass);
